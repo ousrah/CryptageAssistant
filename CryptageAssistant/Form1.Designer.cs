2d7780894e40943c1c3282dd98c0d112cc80aa8d
Oussama Rahmouni <ousrah@hotmail.com>
2022-02-23 10:02:29 +0100

    Ajoutez des fichiers projet.

---
+++ CryptageAssistant/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿
+namespace CryptageAssistant
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.librairieDataSet = new CryptageAssistant.librairieDataSet();
+            this.eCRIVAINBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.eCRIVAINTableAdapter = new CryptageAssistant.librairieDataSetTableAdapters.ECRIVAINTableAdapter();
+            this.tableAdapterManager = new CryptageAssistant.librairieDataSetTableAdapters.TableAdapterManager();
+            this.eCRIVAINBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+            this.eCRIVAINBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
+            this.eCRIVAINDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.librairieDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eCRIVAINBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eCRIVAINBindingNavigator)).BeginInit();
+            this.eCRIVAINBindingNavigator.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.eCRIVAINDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // librairieDataSet
+            // 
+            this.librairieDataSet.DataSetName = "librairieDataSet";
+            this.librairieDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // eCRIVAINBindingSource
+            // 
+            this.eCRIVAINBindingSource.DataMember = "ECRIVAIN";
+            this.eCRIVAINBindingSource.DataSource = this.librairieDataSet;
+            // 
+            // eCRIVAINTableAdapter
+            // 
+            this.eCRIVAINTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.ECRIVAINTableAdapter = this.eCRIVAINTableAdapter;
+            this.tableAdapterManager.UpdateOrder = CryptageAssistant.librairieDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // eCRIVAINBindingNavigator
+            // 
+            this.eCRIVAINBindingNavigator.AddNewItem = this.bindingNavigatorAddNewItem;
+            this.eCRIVAINBindingNavigator.BindingSource = this.eCRIVAINBindingSource;
+            this.eCRIVAINBindingNavigator.CountItem = this.bindingNavigatorCountItem;
+            this.eCRIVAINBindingNavigator.DeleteItem = this.bindingNavigatorDeleteItem;
+            this.eCRIVAINBindingNavigator.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.eCRIVAINBindingNavigator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.bindingNavigatorAddNewItem,
+            this.bindingNavigatorDeleteItem,
+            this.eCRIVAINBindingNavigatorSaveItem});
+            this.eCRIVAINBindingNavigator.Location = new System.Drawing.Point(0, 0);
+            this.eCRIVAINBindingNavigator.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.eCRIVAINBindingNavigator.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.eCRIVAINBindingNavigator.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.eCRIVAINBindingNavigator.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.eCRIVAINBindingNavigator.Name = "eCRIVAINBindingNavigator";
+            this.eCRIVAINBindingNavigator.PositionItem = this.bindingNavigatorPositionItem;
+            this.eCRIVAINBindingNavigator.Size = new System.Drawing.Size(800, 27);
+            this.eCRIVAINBindingNavigator.TabIndex = 0;
+            this.eCRIVAINBindingNavigator.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorMoveFirstItem
+            // 
+            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(29, 22);
+            this.bindingNavigatorMoveFirstItem.Text = "Placer en premier";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(29, 22);
+            this.bindingNavigatorMovePreviousItem.Text = "Déplacer vers le haut";
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorPositionItem
+            // 
+            this.bindingNavigatorPositionItem.AccessibleName = "Position";
+            this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 27);
+            this.bindingNavigatorPositionItem.Text = "0";
+            this.bindingNavigatorPositionItem.ToolTipText = "Position actuelle";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(48, 20);
+            this.bindingNavigatorCountItem.Text = "de {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "Nombre total d\'éléments";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorMoveNextItem.Text = "Déplacer vers le bas";
+            // 
+            // bindingNavigatorMoveLastItem
+            // 
+            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorMoveLastItem.Text = "Placer en dernier";
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorAddNewItem.Text = "Ajouter nouveau";
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorDeleteItem.Text = "Supprimer";
+            // 
+            // eCRIVAINBindingNavigatorSaveItem
+            // 
+            this.eCRIVAINBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.eCRIVAINBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("eCRIVAINBindingNavigatorSaveItem.Image")));
+            this.eCRIVAINBindingNavigatorSaveItem.Name = "eCRIVAINBindingNavigatorSaveItem";
+            this.eCRIVAINBindingNavigatorSaveItem.Size = new System.Drawing.Size(23, 23);
+            this.eCRIVAINBindingNavigatorSaveItem.Text = "Enregistrer les données";
+            this.eCRIVAINBindingNavigatorSaveItem.Click += new System.EventHandler(this.eCRIVAINBindingNavigatorSaveItem_Click);
+            // 
+            // eCRIVAINDataGridView
+            // 
+            this.eCRIVAINDataGridView.AutoGenerateColumns = false;
+            this.eCRIVAINDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.eCRIVAINDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4,
+            this.dataGridViewTextBoxColumn5});
+            this.eCRIVAINDataGridView.DataSource = this.eCRIVAINBindingSource;
+            this.eCRIVAINDataGridView.Location = new System.Drawing.Point(99, 106);
+            this.eCRIVAINDataGridView.Name = "eCRIVAINDataGridView";
+            this.eCRIVAINDataGridView.RowHeadersWidth = 51;
+            this.eCRIVAINDataGridView.RowTemplate.Height = 24;
+            this.eCRIVAINDataGridView.Size = new System.Drawing.Size(300, 220);
+            this.eCRIVAINDataGridView.TabIndex = 1;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "NUMECR";
+            this.dataGridViewTextBoxColumn1.HeaderText = "NUMECR";
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "PRENOMECR";
+            this.dataGridViewTextBoxColumn2.HeaderText = "PRENOMECR";
+            this.dataGridViewTextBoxColumn2.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "NOMECR";
+            this.dataGridViewTextBoxColumn3.HeaderText = "NOMECR";
+            this.dataGridViewTextBoxColumn3.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "PAYSECR";
+            this.dataGridViewTextBoxColumn4.HeaderText = "PAYSECR";
+            this.dataGridViewTextBoxColumn4.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.DataPropertyName = "LANGUECR";
+            this.dataGridViewTextBoxColumn5.HeaderText = "LANGUECR";
+            this.dataGridViewTextBoxColumn5.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            this.dataGridViewTextBoxColumn5.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.eCRIVAINDataGridView);
+            this.Controls.Add(this.eCRIVAINBindingNavigator);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.librairieDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eCRIVAINBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eCRIVAINBindingNavigator)).EndInit();
+            this.eCRIVAINBindingNavigator.ResumeLayout(false);
+            this.eCRIVAINBindingNavigator.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.eCRIVAINDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private librairieDataSet librairieDataSet;
+        private System.Windows.Forms.BindingSource eCRIVAINBindingSource;
+        private librairieDataSetTableAdapters.ECRIVAINTableAdapter eCRIVAINTableAdapter;
+        private librairieDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.BindingNavigator eCRIVAINBindingNavigator;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.ToolStripButton eCRIVAINBindingNavigatorSaveItem;
+        private System.Windows.Forms.DataGridView eCRIVAINDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+    }
+}
+
---
+++ CryptageAssistant/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CryptageAssistant
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void eCRIVAINBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.eCRIVAINBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.librairieDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: cette ligne de code charge les données dans la table 'librairieDataSet.ECRIVAIN'. Vous pouvez la déplacer ou la supprimer selon les besoins.
+            this.eCRIVAINTableAdapter.Fill(this.librairieDataSet.ECRIVAIN);
+
+        }
+    }
+}
